22e8a8cedd2328219e22bea02e955cd8ccf09791
Eric G. Alessandrini <eric.alessandrini1@gmail.com>
2025-02-19 14:05:21 -0300

    Inicio de proyecto de ToDoApp

---
+++ src/main/java/com/ega/to_do_app/ToDoAppApplication.java
@@ -0,0 +1,14 @@
+package com.ega.to_do_app;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppApplication.class, args);
+	}
+}
